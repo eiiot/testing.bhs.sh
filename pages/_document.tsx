b52bf325ae77c8097cf3301df4aff9dc46cf2e56
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2023-04-30 16:27:17 -0700

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="testing.bhs.sh"
+          src="https://analytics.eliothertenstein.com/js/plausible.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
