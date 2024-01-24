96802789915c981fc3446709f38294c7c01ddc19
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2024-01-24 13:51:10 -0800

    fix time shifting issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Schedule from "@/components/Schedule";
-import { Metadata } from 'next'
+import { Metadata } from "next";
 import { bhsExams } from "@/data/2024";
 
-
 export const metadata: Metadata = {
-  title: 'BHS Testing Schedule',
-}
+  title: "BHS Testing Schedule",
+};
 
-const days = new Array(20).fill(0).map((_, i) => new Date(2024, 4, 1 + i));
+const pad = (n: number) => (n < 10 ? "0" + n : n);
+
+const days = new Array(17)
+  .fill(0)
+  .map((_, i) => new Date(`2024-05-${pad(1 + i)}T00:00:00-07:00`));
+
+console.log("Time Period:", days[0], "to", days[days.length - 1]);
 
 export default function Home() {
   return <Schedule days={days} exams={bhsExams} link="/2023" year={2024} />;
